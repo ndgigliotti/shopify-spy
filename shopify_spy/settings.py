12affbbeac5df7fcbcdc821cd1eeaee32849f0f3
Nicholas Gigliotti <ndgigliotti@gmail.com>
2026-01-25 23:20:27 -0500

    Change default output directory to output/ and enable autothrottle

--- shopify_spy/settings.py
+++ shopify_spy/settings.py
@@ -21,11 +21,11 @@ CONCURRENT_REQUESTS_PER_DOMAIN = 16
 COOKIES_ENABLED = False
 TELNETCONSOLE_ENABLED = False
 
-RESOURCES_DIR = Path(__file__).resolve().parent.parent / "resources"
-RESOURCES_URI = RESOURCES_DIR.as_uri()
+OUTPUT_DIR = Path(__file__).resolve().parent.parent / "output"
+OUTPUT_URI = OUTPUT_DIR.as_uri()
 
 FEEDS = {
-    f"{RESOURCES_URI}/%(name)s/%(time)s.json": {
+    f"{OUTPUT_URI}/%(name)s_%(time)s.json": {
         "format": "jsonlines",
         "encoding": "utf8",
         "store_empty": False,
@@ -34,15 +34,13 @@ FEEDS = {
     },
 }
 
-FEED_URI_PARAMS = "shopify_spy.utils.uri_params"
-
 ITEM_PIPELINES = {"scrapy.pipelines.images.ImagesPipeline": 1}
-IMAGES_STORE = str(RESOURCES_DIR / "images")
+IMAGES_STORE = str(OUTPUT_DIR / "images")
 IMAGES_RESULT_FIELD = "scraped_images"
 
-# Enable AutoThrottle to avoid hitting servers too hard
+# AutoThrottle - enabled by default for polite scraping
 # https://docs.scrapy.org/en/latest/topics/autothrottle.html
-# AUTOTHROTTLE_ENABLED = True
-# AUTOTHROTTLE_START_DELAY = 5
-# AUTOTHROTTLE_MAX_DELAY = 60
-# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 1
+AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
